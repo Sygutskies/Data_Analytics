98e40b86ec0221c1f1b5a0869aece3bbc7a4e25d
Sygutskies <adamsygut16@gmail.com>
2022-03-08 20:19:23 +0100

    ex_1_done

--- ex1.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-
-df = pd.read_csv('Data1.csv')
-df.rename( columns={'Unnamed: 0':'Date'}, inplace=True )
-df.set_index('Date')
-plt.plot('Date', 'theta_1')
